9391cc218bafea899ab027eac22c57c32ebe7f67
ccarabine <ccarabine12@gmail.com>
2021-12-02 14:07:18 +0000

    Addd likes section

--- blog/admin.py
+++ blog/admin.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
-from django_summernote.admin import SummernoteModelAdmin
 from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
+
     list_display = ('title', 'slug', 'status', 'created_on')
     search_fields = ['title', 'content']
-    prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'created_on')
-    summernote_fields = ('content')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 
 
 @admin.register(Comment)
@@ -17,7 +18,7 @@ class CommentAdmin(admin.ModelAdmin):
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
     search_fields = ('name', 'email', 'body')
-    actions = ['approved_comments']
-    
+    actions = ['approve_comments']
+
     def approve_comments(self, request, queryset):
         queryset.update(approved=True)
